220dec017c508e9ca6baee8efc277b6167bd3a3f
Marcelo Lins <marcelolinsdemedeiros@gmail.com>
2022-04-06 18:26:09 -0300

    Refatorando e aprimorando os método valorTotal e adicionarItem

--- src/com/lins/Pedido.java
+++ src/com/lins/Pedido.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pedido {
-	//refatorar método
+	
 	private double desconto;
 	
 	private List<ItemPedido> itens = new ArrayList<>();
@@ -14,7 +14,6 @@ public class Pedido {
 	}
 
 	public double valorTotal() {
-	//	valorTotal += ItemPedido.getValUnitario() * ItemPedido.getQtd();
 	return itens.stream().mapToDouble(i -> i.getValUnitario() * i.getQtd()).sum();
 	
 	}
